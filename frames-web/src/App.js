3c1e93efc8434dbfdab0d89b3803a3b5a7e7c364
Ivan Bertona <ivan@ibrt.me>
2020-04-10 14:28:44 -0400

    Update copy.

--- frames-web/src/App.js
+++ frames-web/src/App.js
@@ -15,10 +15,10 @@ export default function App() {
           px={[ 2, null, 4 ]}>
         <Header/>
         <Box pb={[ 3, null, 4 ]} sx={{ textAlign: 'center' }}>
-          <Heading as='h2' variant='title'>Add Photo Frame to All Your Social Accounts</Heading>
+          <Heading as='h2' variant='title'>Add a frame to all your Social profiles</Heading>
           <Heading as='h3' variant='subtitle'><b>Step 1</b> - Upload and adjust photo</Heading>
           <Heading as='h3' variant='subtitle'><b>Step 2</b> - Select language and choose frame</Heading>
-          <Heading as='h3' variant='subtitle'><b>Step 3</b> - Download new profile photo or upload directly to Facebook</Heading>
+          <Heading as='h3' variant='subtitle'><b>Step 3</b> - Download new profile photo or use Facebook uploader</Heading>
         </Box>
         <Box>
           <Editor/>
